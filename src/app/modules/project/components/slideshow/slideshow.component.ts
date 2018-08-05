23a3c05992472f7f0c07cb62b9db7815c4685789
Wind <ao6swind@hotmail.com>
2018-08-05 16:59:43 +0800

    新增專案說明與翻譯, 修改bulma extension的匯入與使用

--- src/app/modules/project/components/slideshow/slideshow.component.ts
+++ src/app/modules/project/components/slideshow/slideshow.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Carousel } from './../../../../plugin/carousel';
+import { bulmaCarousel } from 'bulma-extensions/';
+//import { Carousel } from './../../../../plugin/carousel';
 @Component({
   selector: 'app-slideshow',
   templateUrl: './slideshow.component.html',
@@ -16,11 +17,17 @@ export class SlideshowComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    /******************************************
+     * 20180805: 
+     * 不知道bulma哪次更新大改extension的html與js邏輯
+     * 偷懶就直接import他們release出來的js
     let carousels = document.querySelectorAll('.carousel, .hero-carousel');
     if (carousels) {
       for(let i = 0; i <= carousels.length - 1; i++){
         new Carousel(carousels[i]);
       }
     }
+     ******************************************/
+    var carousels = bulmaCarousel.attach();
   }
 }
